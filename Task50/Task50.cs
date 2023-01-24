8d1d18683a77d685c13ca9b9fc8dc5f1b374b54a
Nickolay Grigoriev <gnarus@inbox.ru>
2023-01-24 21:35:24 +0300

    first commit

---
+++ Task50/Task50.cs
@@ -0,0 +1,66 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет. 
+Например, задан массив:
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+
+Console.Clear();
+
+int[,] FillMatrix(int[,] matr) // метод заполнения и вывода на экран матрицы
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(-10, 11);
+            Console.Write($"{matr[i, j]} "); // вывод на экран результата генерации
+        }
+        Console.WriteLine();
+    }
+    return matr;
+}
+
+int InputNumber() // ввод позиции элемента
+{
+    int num;
+    while (true)
+    {
+        Console.WriteLine("Введите число");
+        if (int.TryParse(Console.ReadLine(), out num))
+            break;
+        Console.WriteLine("Ошибка ввода");
+    }
+    return num;
+}
+
+void PrintMatrix(int[,] matr, int nums) // поиск элемента значения по номеру и вывод на экран результата поиска
+{
+    int count = 0;
+    bool flag = false;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            count++;
+            if (count == nums)
+            {
+                Console.WriteLine($"На позиции {count} -> {matr[i, j]}");
+                flag = true;
+            }
+        }
+        Console.WriteLine();
+    }
+    if (flag == false) Console.WriteLine("такого числа в массиве нет");
+}
+
+int num = InputNumber();
+int[,] matrix = new int[3, 4];
+matrix = FillMatrix(matrix); // обращение к методу генерирующий матрицу и выводящий ее на консоль
+PrintMatrix(matrix, num); // поиск элемента в матрице
